89c48e5af4622771617ae3f3779dd0faff9980e7
MariaFL <fedmaria2@gmail.com>
2021-04-06 21:48:03 +0500

    init project, add webpack

---
+++ webpack/webpack.dev.conf.js
@@ -0,0 +1,18 @@
+const {merge} = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
+
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    port: 8081,
+  }
+});
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig)
+});
